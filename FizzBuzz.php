3178521114b7de706f8db6da06c87bcfd5ce9e7c
takaakiyamaga <39820992+takaakiyamaga@users.noreply.github.com>
2018-06-11 19:39:36 +0900

    修正

--- FizzBuzz.php
+++ FizzBuzz.php
@@ -1,15 +1,16 @@
 <?php
-for ( $e = 1; $e <= 339; $e++ ) {
-    if ( $e % 5 == 0 ) {
-      echo 'Fizz<br/>';
+for ( $e = 1; $e <= 100; $e++ ) {
+    if ( $e % 15 == 0) {
+
+
+        echo 'FizzBuzz<br/>';
 
     } else if ( $e % 5 == 0) {
 
         echo 'Buzz<br/>';
 
-    } else if (  $e % 15 == 0) {
-
-        echo 'FizzBuzz<br/>';
+    } else if (  $e % 3 == 0 ) {
+      echo 'Fizz<br/>';
 
     } else {
         echo $e;
